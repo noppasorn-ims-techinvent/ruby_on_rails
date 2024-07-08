d67244ef17e6365b414ea59c8237036895d7db89
noppasorn-ims-techinvent <noppasorn.ims@techinvent.in.th>
2024-07-08 11:39:11 +0700

    add users show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
-end
+    def gravatar_for(user, size: 150) 
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      image_tag(gravatar_url, alt: user.username, class: "gravatar rounded shadow mx-auto p-2", style: "border: none;")
+    end
+  end
+  
